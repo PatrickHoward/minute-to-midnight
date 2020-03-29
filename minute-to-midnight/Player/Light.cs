58a016c6360221717f8e68fbc5442a1d9bcaa9a6
link00000000 <crandall.logan@gmail.com>
2020-03-29 14:45:15 -0400

    Fix flickering without dimming causing infinite energy

--- minute-to-midnight/Player/Light.cs
+++ minute-to-midnight/Player/Light.cs
@@ -1,15 +1,61 @@
 using System;
 using Godot;
 
+public class LightStrength
+{
+    private Tuple<float, float> _energyLimits;
+    private Tuple<float, float> _scaleLimits;
+
+    private float _energy;
+    private float _scale;
+
+    public LightStrength(Tuple<float, float> energyLimits, Tuple<float, float> scaleLimits)
+    {
+        _energyLimits = energyLimits;
+        _scaleLimits = scaleLimits;
+
+        _energy = energyLimits.Item2;
+        _scale = scaleLimits.Item2;
+    }
+
+    private Vector2 GetScaleAsVector2()
+    {
+        return new Vector2(Scale, Scale);
+    }
+
+    public float Energy
+    {
+        get { return _energy; }
+        set
+        {
+            _energy = Mathf.Clamp(value, _energyLimits.Item1, _energyLimits.Item2);
+        }
+    }
+
+    public float Scale
+    {
+        get { return _scale; }
+        set
+        {
+            _scale = Mathf.Clamp(value, _scaleLimits.Item1, _scaleLimits.Item2);
+        }
+    }
+
+    public float Flicker { get; set; } = 1.0f;
+
+    public void Apply(Light2D light2DNode)
+    {
+        light2DNode.Energy = Energy * Flicker;
+        light2DNode.SetScale(GetScaleAsVector2());
+    }
+}
+
 public class Light : KinematicBody2D
 {
-    private readonly float _maxScale = 1.0f;
-    private readonly float _minScale = 0.1f;
-    private readonly float _maxEnergy = 1.0f;
-    private readonly float _minEnergy = 0.0f;
+    private readonly Tuple<float, float> _scaleLimits = Tuple.Create(0.1f, 1.0f);
+    private readonly Tuple<float, float> _energyLimits = Tuple.Create(0.0f, 1.0f);
+    private readonly Tuple<float, float> _flickerLimits = Tuple.Create(1.0f, 1.1f);
     private readonly float _maxTime = 60;
-    private float _maxFlicker = 1.1f;
-    private float _minFlicker = 1.0f;
 
     [Export]
     public bool Debug = false;
@@ -37,6 +83,8 @@ public class Light : KinematicBody2D
 
     private Light2D _lightSource;
 
+    private LightStrength _lightStrength;
+
     private Vector2 GetInput()
     {
         return new Vector2(
@@ -45,30 +93,19 @@ public class Light : KinematicBody2D
         ).Normalized();
     }
 
-    private void setStrength()
-    {
-        var percentTimeRemaining = _timeRemaining / Duration;
-
-        var energy = percentTimeRemaining * (_maxEnergy - _minEnergy) + _minEnergy;
-        _lightSource.Energy = percentTimeRemaining = energy;
-
-        var scale = percentTimeRemaining * (_maxScale - _minScale) + _minScale;
-        _lightSource.SetScale(new Vector2(scale, scale));
-    }
-
     public override void _Ready()
     {
         _timer = GetNode<Timer>("Timer");
         _lightSource = GetNode<Light2D>("Light2D");
         _timeRemaining = Duration;
 
-        if (!DisableDimming)
+        _lightStrength = new LightStrength(_energyLimits, _scaleLimits);
+        _lightStrength.Apply(_lightSource);
+
+        if (!DisableDimming || Flicker)
         {
             _timer.Start();
         }
-
-        _lightSource.Energy = _maxEnergy;
-        _lightSource.SetScale(new Vector2(_maxScale, _maxScale));
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -83,11 +120,25 @@ public class Light : KinematicBody2D
 
     public void _on_Timer_timeout()
     {
-        _timeRemaining = Mathf.Max(_timeRemaining - _timer.WaitTime, 0);
-        setStrength();
+        if (Flicker)
+        {
+            var noise = (float)(new Random()).NextDouble() * (_flickerLimits.Item2 - _flickerLimits.Item1) + _flickerLimits.Item1;
+            _lightStrength.Flicker = noise;
+        }
 
-        var noise = (float)(new Random()).NextDouble() * (1.1f - 1.0f) + 1.0f;
-        _lightSource.Energy *= noise;
+        if (!DisableDimming)
+        {
+            GD.Print("DIMMING");
+            _timeRemaining = Mathf.Max(_timeRemaining - _timer.WaitTime, 0);
+            var percentTimeRemaining = _timeRemaining / Duration;
+
+            _lightStrength.Energy = percentTimeRemaining * (_energyLimits.Item2 - _energyLimits.Item1) + _energyLimits.Item1;
+            _lightStrength.Scale = percentTimeRemaining * (_scaleLimits.Item2 - _scaleLimits.Item1) + _scaleLimits.Item1;
+
+            GD.Print(percentTimeRemaining);
+        }
+
+        _lightStrength.Apply(_lightSource);
 
         if (_timeRemaining == 0)
         {
