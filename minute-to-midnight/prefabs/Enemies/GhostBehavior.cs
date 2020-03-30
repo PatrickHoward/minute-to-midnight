e70136a64ce7e06d5a53d45a64b7d9c885756ff8
Patrick M. Howard <pmontgomeryhoward@gmail.com>
2020-03-30 15:54:16 -0400

    Add ghost enemy

--- minute-to-midnight/prefabs/Enemies/GhostBehavior.cs
+++ minute-to-midnight/prefabs/Enemies/GhostBehavior.cs
@@ -1,12 +1,13 @@
 using System;
 using Godot;
+using Array = Godot.Collections.Array;
 
 public enum EnemyAnimationState
 {
 	Idle,
 	Walk,
 	Death,
-	Attack1,
+	Attack1
 }
 
 public enum EnemyState
@@ -14,12 +15,16 @@ public enum EnemyState
 	Idle,
 	Walking,
 	Attacking,
+	Dead
 }
 
 public class EnemyBehavior00 : KinematicBody2D
 {
 	[Export] public float Speed = 50;
+	[Export] public float AttackSpeed = 100;
 	[Export] public float Gravity = 9.8f;
+	[Export] public float Damage = 5f;
+	[Export] public int HitsToDestroy = 1;
 	
 	private int _changedirection = -1;
 	
@@ -33,17 +38,31 @@ public class EnemyBehavior00 : KinematicBody2D
 	
 	private EnemyAnimationState _animationState;
 	private AnimatedSprite _animations;
-	private RayCast2D _rc;
+	private Node2D _display;
+	
+	private RayCast2D _groundCheck;
+	private RayCast2D _attackCheck;
+	private Area2D _damageArea;
 	
 	public override void _Ready()
 	{
 		_movement = new Vector2();
-		_animations = GetChild<Node>(1).GetChild<AnimatedSprite>(0);
-		_rc = GetChild<RayCast2D>(2);
+		_animations = GetNode<AnimatedSprite>("Display/AnimatedSprite");
+
+		_display = GetNode<Node2D>("Display");
+		
+		_groundCheck = GetNode<RayCast2D>("GroundCheck");
+		_attackCheck = GetNode<RayCast2D>("Display/AttackCheck");
+		_damageArea = GetNode<Area2D>("DamageArea");
 	}
 
 	public override void _Process(float delta)
 	{
+		if (HitsToDestroy <= 0)
+		{
+			_state = EnemyState.Dead;
+		}
+		
 		UpdatePlayerState();
 		UpdateAnimation();
 	}
@@ -51,20 +70,29 @@ public class EnemyBehavior00 : KinematicBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(float delta)
 	{
-		
-
 		if (_state == EnemyState.Idle)
 		{
 			_state = EnemyState.Walking;
 		}
-		
-		if(IsOnWall() || !_rc.IsColliding())
+
+		if (_state == EnemyState.Dead)
 		{
-			Speed *= _changedirection;
-			_rc.Position *= new Vector2(-1,1);
+			return;
 		}
 		
-		_movement.x = Speed;
+		if(IsOnWall() || !_groundCheck.IsColliding())
+		{
+			Speed *= _changedirection;
+			_groundCheck.Position *= new Vector2(-1,1);
+		}
+
+		if (_attackCheck.IsColliding())
+		{
+			var body = _attackCheck.GetCollider().GetClass();
+		}
+		
+		
+		_movement.x = _attacking ? AttackSpeed : Speed;
 		
 		_movement.y = Gravity;
 		
@@ -82,14 +110,21 @@ public class EnemyBehavior00 : KinematicBody2D
 		{
 			case EnemyState.Idle:
 				_animationState = EnemyAnimationState.Idle;
+				_attacking = false;
 				break;
 			
 			case EnemyState.Attacking:
 				_animationState = EnemyAnimationState.Attack1;
+				_attacking = true;
 				break;
 			
 			case EnemyState.Walking:
 				_animationState = EnemyAnimationState.Walk;
+				_attacking = false;
+				break;
+			
+			case EnemyState.Dead :
+				_animationState = EnemyAnimationState.Death;
 				break;
 		}
 	}
@@ -98,11 +133,11 @@ public class EnemyBehavior00 : KinematicBody2D
 	{
 		if (_movement.x < 0)
 		{
-			_animations.FlipH = true;
+			_display.Scale = new Vector2(-1, 1);
 		}
 		else if (_movement.x > 0)
 		{
-			_animations.FlipH = false;
+			_display.Scale = new Vector2(1, 1);
 		}
 
 		switch (_animationState)
@@ -125,15 +160,37 @@ public class EnemyBehavior00 : KinematicBody2D
 		}
 	}
 
-	public void _on_Sprite_animation_finished()
+	public void DealDamageToEnemy()
+	{
+		Console.WriteLine("Dealt damage!");
+		--HitsToDestroy;
+	}
+
+	public void _on_AnimatedSprite_animation_finished()
 	{
 		if (_movement == Vector2.Zero && IsOnFloor())
 		{
 			_state = EnemyState.Idle;
 		}
+		
 		else if (IsOnFloor())
 		{
 			_state = EnemyState.Walking;
+		}
+
+		if (_state == EnemyState.Dead || _animationState == EnemyAnimationState.Death)
+		{
+			QueueFree();
+		}
+		
+	}
+
+	public void _on_DamageArea_body_entered(Node body)
+	{
+		if (body.Name == "Player")
+		{
+			float[] timeArg = {Damage};
+			body.PropagateCall(nameof(Light.RemoveTimeFromTimer), new Array(timeArg));
 		}
 	}
 }
