2674c3c4a3fb7fa24f382e877364bc86497ca2fe
Mason C. Roberts <mcr66@zips.uakron.edu>
2020-03-31 15:05:18 -0400

    Level Expansion one

--- minute-to-midnight/scenes/menu/MainMenu.cs
+++ minute-to-midnight/scenes/menu/MainMenu.cs
@@ -3,36 +3,36 @@ using System;
 
 public class MainMenu : Control
 {
-    private AnimationPlayer _animationPlayer;
-    
-    public override void _Ready()
-    {
-        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-    }
+	private AnimationPlayer _animationPlayer;
+	
+	public override void _Ready()
+	{
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
 
-    public void Quit()
-    {
-        GetTree().Quit();
-    }
+	public void Quit()
+	{
+		GetTree().Quit();
+	}
 
-    public void StartGame()
-    {
-        GetTree().ChangeScene("res://scenes/levels/Level1/Level1.tscn");
-    }
-    
-    public void _on_Start_Game_Trigger_body_entered(Node body)
-    {
-        if (body.Name == "Player")
-        {
-            _animationPlayer.Play("Fade_Into_Game");
-        }
-    }
-    
-    public void _on_Exit_Game_Trigger_body_entered(Node body)
-    {
-        if (body.Name == "Player")
-        {
-            _animationPlayer.Play("Fade_Exit");
-        }
-    }
+	public void StartGame()
+	{
+		GetTree().ChangeScene("res://scenes/levels/Level1/Level1.tscn");
+	}
+	
+	public void _on_Start_Game_Trigger_body_entered(Node body)
+	{
+		if (body.Name == "Player")
+		{
+			_animationPlayer.Play("Fade_Into_Game");
+		}
+	}
+	
+	public void _on_Exit_Game_Trigger_body_entered(Node body)
+	{
+		if (body.Name == "Player")
+		{
+			_animationPlayer.Play("Fade_Exit");
+		}
+	}
 }
