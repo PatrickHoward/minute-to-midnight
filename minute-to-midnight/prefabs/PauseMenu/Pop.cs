531359c03f045102bbc1e4149ba533556989237a
Mason C. Roberts <mcr66@zips.uakron.edu>
2020-04-07 20:45:20 -0400

    Final touches for branch

--- minute-to-midnight/prefabs/PauseMenu/Pop.cs
+++ minute-to-midnight/prefabs/PauseMenu/Pop.cs
@@ -3,7 +3,16 @@ using System;
 
 public class Pop : Popup
 {
-
+	public override void _Process(float delta)
+	{
+		if(Input.IsActionPressed("Okay"))
+		{
+			Visible = false;
+			GetNode<ColorRect>("CanvasLayer/ColorRect").Visible = false;
+			GetTree().Paused = false;
+		}
+	}
+	
 	private void _on_ToolButton_button_down()
 	{
 		Visible = false;
