44be9bd8db4920484085f5ce229a280cb048d621
Mason C. Roberts <mcr66@zips.uakron.edu>
2020-04-10 16:00:26 -0400

    Implement full logic for volume sliders

--- minute-to-midnight/prefabs/PauseMenu/Pause.cs
+++ minute-to-midnight/prefabs/PauseMenu/Pause.cs
@@ -27,7 +27,7 @@ public class Pause : Control
 	
 	public override void _Input(InputEvent e)
 	{
-		if(e.IsActionPressed("Pause"))
+		if(e.IsActionPressed("Pause") )
 		{
 			_pauseState = !GetTree().Paused;
 			GD.Print("Pause State: " + _pauseState);
@@ -35,6 +35,9 @@ public class Pause : Control
 			Visible = _pauseState;
 			_screen.Visible = _pauseState;
 			_menu.Visible = _pauseState;
+			
+			if(_opMenu.Visible == true)
+				_opMenu.Visible = false;
 		}
 	}
 
@@ -68,6 +71,15 @@ public class Pause : Control
 		SettingsData.MasterVolume = _masterVolume;
 		SettingsData.MusicVolume = _musicVolume; 
 		SettingsData.MonsterVolume = _monsterVolume;
+		
+		foreach(AudioStreamPlayer node in GetTree().GetNodesInGroup("Ao-Music"))
+			node.VolumeDb = _musicVolume;
+			
+		foreach(AudioStreamPlayer2D node in GetTree().GetNodesInGroup("Ao-Monsters"))
+			node.VolumeDb = _monsterVolume;
+			
+		foreach(AudioStreamPlayer2D node in GetTree().GetNodesInGroup("Ao-Master"))
+			node.VolumeDb = _masterVolume;
 	}
 	
 	private void _on_Quit_button_down()
